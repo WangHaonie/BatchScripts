0bc3c34ea7e3b38c65c9961ba2db8b1c10172e9b
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2023-08-30 08:21:45 +0800

    更新了部分脚本，详见 Commit
    VideoDurationCalc：
    + 调整了一些样式
    
    XyybsDownloader：
    + 更新到了 v3.0；
    + 支持含有多个文件的资源码，以自动添加序号；
    + 当网页 <title> 元素内容和资源无关时，则使用 <h3> 元素的内容，以确保文件名和资源相关；
    + 调整了一些样式。

--- VideoDurationCalc/VideoDurationCalc.ps1
+++ VideoDurationCalc/VideoDurationCalc.ps1
@@ -10,7 +10,7 @@ Write-Host "该工具可以计算开了倍速后的视频，需要多久才能播放完"
 if ((!$videoTime -or !$playbackSpeed) -or ($videoTime -and !$playbackSpeed)) {
     Write-Host " "
     Write-Host "缺少相关参数，已进入交互模式运行，请正确地指定相关参数" -ForegroundColor Red
-    Write-Host "格式：watchtime.ps1 <视频时长 HH:mm:ss> <播放倍速>" -ForegroundColor Red
+    Write-Host "格式：vdc <视频时长 HH:mm:ss> <播放倍速>" -ForegroundColor Red
     Write-Host " "
     $videoTime = Read-Host "请输入视频时长（格式 时:分:秒）"
     $playbackSpeed = Read-Host "请输入播放倍速，比如2倍速则输入2即可"
